8738c763f5980ffdfa7e9b16387f9bb5ea979c02
Matheus Cassiano <matheuscassiano6467@gmail.com>
2023-02-04 13:04:59 -0300

    Exception handling - delete

--- src/main/java/com/matheuscassiano/course/service/UserService.java
+++ src/main/java/com/matheuscassiano/course/service/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.matheuscassiano.course.entities.User;
 import com.matheuscassiano.course.repositories.UserRepository;
+import com.matheuscassiano.course.service.exceptions.DatabaseException;
 import com.matheuscassiano.course.service.exceptions.ResourceNotFoundException;
 //registra a classe como componente, permitindo que o autowired o injete automaticamente
 //@Component
@@ -33,7 +36,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	public User update(Long id, User obj) {
 		//prepara o objeto monitorado para mexer e depois efetuar uma operação com o bd
